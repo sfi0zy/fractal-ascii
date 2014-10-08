521532b17e7de0354e36c79a0ab3d1daef7a12d5
sfi0zy <sfi0zy@gmail.com>
2014-10-08 10:54:11 +0400

    Add threads

--- fractal.c
+++ fractal.c
@@ -2,65 +2,99 @@
 #include <string.h>
 #include <stdbool.h>
 #include <ncurses.h>
+#include <pthread.h>
 
 
 #define MAX_ITERATIONS 1024
 #define ESCAPE_RADIUS  4.02
 
+#define GAMMA_ASCII " .`-_':,;^=+/\"|)\\<>)iv%xclrs{*}I?!][1taeo7zjLunT#JCwfy325Fp6mqSghVd4EgXPGZbYkOA&8U$@KHDBWNMR0Q"
+#define NUMBER_OF_COLORS 7
 
-const char* letters = " .`-_':,;^=+/\"|)\\<>)iv%xclrs{*}I?!][1taeo7zjLunT#JCwfy325Fp6mqSghVd4EgXPGZbYkOA&8U$@KHDBWNMR0Q";
+
+typedef struct {
+	double cam_x;
+	double cam_y;
+	double zoom;
+	unsigned int iterations;
+} fractal_params_t;
 
 
-unsigned int mandelbrot_set(double x0, double y0);
+void* render_image(void*);
+unsigned int mandelbrot_set(double, double);
 void init_color_pairs();
-void print_char(int x, int y, unsigned long char_param, int color_pair);
+void print_char(int, int, unsigned long, int);
 
 
 int main(void)
 {
-	double x, y, x0, y0, cam_x, cam_y, zoom;
-	unsigned int iterations, pressed_key;
-	const unsigned int letters_length = strlen(letters);
+	unsigned int pressed_key;
+	fractal_params_t params;
+	pthread_t render_thread_id;
 
-	cam_x = cam_y = 0.0;
-	zoom = 10.0;
+	params.cam_x = 0.0;
+	params.cam_y = 0.0;
+	params.zoom  = 10.0;
 
 	initscr();
 	noecho();
 	keypad(stdscr, true);
-	start_color();
-	init_color_pairs();
+
+	pthread_create(&render_thread_id, NULL, &render_image, &params);
 
 	do {
-		for (int y = 0; y < LINES; ++y) {
-			y0 = (((LINES / 2) - y) / zoom) + cam_y;
-
-			for (x = 0; x < COLS; ++x) {
-				x0 = ((x - (COLS / 2)) / zoom) / 2 + cam_x;
-				iterations = mandelbrot_set(x0, y0);
-
-				print_char(x, y, (unsigned long)letters[iterations % letters_length] | A_BOLD,
-					(int)(((iterations * 128) / MAX_ITERATIONS) % 7 + 1));
-			}
-		}
-
-		refresh();
 		pressed_key = getch();
 
 		switch(pressed_key) {
-			case 'w': zoom *= 1.2; break;
-			case 's': zoom /= 1.2; break;
-			case KEY_UP:    cam_y += 1 / zoom; break;
-			case KEY_DOWN:  cam_y -= 1 / zoom; break;
-			case KEY_LEFT:  cam_x -= 1 / zoom; break;
-			case KEY_RIGHT: cam_x += 1 / zoom; break;
+			case 'w': params.zoom *= 1.2; break;
+			case 's': params.zoom /= 1.2; break;
+			case KEY_UP:    params.cam_y += 1 / params.zoom; break;
+			case KEY_DOWN:  params.cam_y -= 1 / params.zoom; break;
+			case KEY_LEFT:  params.cam_x -= 1 / params.zoom; break;
+			case KEY_RIGHT: params.cam_x += 1 / params.zoom; break;
 		}
 
 	} while (pressed_key != 'q');
 
 
 	endwin();
+	clear();
 	exit(0);
+}
+
+
+void* render_image(void* fractal_params)
+{
+	int x, y;
+	double x0, y0, cam_x, cam_y, zoom;
+	unsigned int iterations;
+
+	const unsigned int gamma_length = strlen(GAMMA_ASCII);
+
+	start_color();
+	init_color_pairs();
+
+	while (true) {
+		cam_x = ((fractal_params_t*)fractal_params)->cam_x;
+		cam_y = ((fractal_params_t*)fractal_params)->cam_y;
+		zoom  = ((fractal_params_t*)fractal_params)->zoom;
+
+		for (y = 0; y < LINES; ++y) {
+			y0 = (((LINES / 2) - y) / zoom) + cam_y;
+
+			for (x = 0; x < COLS; ++x) {
+				x0 = ((x - (COLS / 2)) / zoom) / 2 + cam_x;
+				iterations = mandelbrot_set(x0, y0);
+
+				print_char(x, y, (unsigned long)GAMMA_ASCII[iterations % gamma_length] | A_BOLD,
+					(int)(((iterations * 128) / MAX_ITERATIONS) % NUMBER_OF_COLORS + 1));
+			}
+		}
+
+		refresh();
+	}
+
+	return NULL;
 }
 
 
